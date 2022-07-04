071072e4a3a1a08cd7bd7904fd7d7be0f626f7f4
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-07-04 20:25:28 +0530

    session 2
    - useEffect, componentDidMount
    - axios
    - modal

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,38 @@
-import React from "react";
-import { FlatList, View, Text, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
+import React, { FC, useEffect, useState } from "react";
+import { FlatList, View, Text, StyleSheet, Image, Dimensions, ScrollView, ActivityIndicator, TouchableOpacity, Modal, InteractionManager } from "react-native";
 
-import { list } from "../utils/data";
+
+import http from "../utils/http";
+import Button from "./Button";
 
 const List: React.FC = () => {
 
-  const renderUser = (item: any) => {
+  const [userList, setUserList] = useState([])
+  const [error, setError] = useState(false)
+  const [selectedUser, setSelectedUser] = useState({})
+
+  const [showModal, setShowModal] = useState(false)
+
+  const getUserList =  async() => {
+    http.get("api/?results=50")
+    .then(res=> setUserList(res.data.results))
+    .catch(err => setError(true))
+  }
+
+  useEffect(()=>{
+    getUserList()
+  },[])
+
+  const renderUser = ({item}: any) => {
     return(
-      <View style={styles.row} key={item.email}>
+      <TouchableOpacity
+        onPress={()=> {
+          setShowModal(true);
+          setSelectedUser(item)
+        }}
+        style={styles.row}
+        key={item.email}
+      >
         <View>
           <Image source={{ uri: item.picture.thumbnail }} style={styles.image} />
         </View>
@@ -21,23 +46,61 @@ const List: React.FC = () => {
             {item.location.city}, {item.location.state}, {item.location.country}
           </Text>
         </View>
-      </View>
+      </TouchableOpacity>
     );
+  }
+
+  const TheModal : FC = () => {
+   return <Modal
+     visible={showModal}
+     transparent
+    >
+      <View style={styles.modalConainer}>
+      <Button 
+       title="❌"
+       onPress={()=>{
+        setShowModal(false)
+        setSelectedUser({})
+        }} 
+        style={styles.modalBtnStyle}
+      />
+      <View>
+          <Image source={{ uri: selectedUser?.picture?.thumbnail }} style={styles.image} />
+        </View>
+      <View>
+          <Text style={[styles.nameText, styles.modalName]}>{selectedUser?.name?.first} {selectedUser?.name?.last}</Text>
+          <Text style={[styles.emailText, styles.modalEmail]}>{selectedUser?.email}</Text>
+          <Text
+            style={[styles.locationText, styles.modalAddress]}
+            numberOfLines={1}
+          >
+            {selectedUser?.location?.city}, {selectedUser?.location?.state}, {selectedUser?.location?.country}
+          </Text>
+        </View>
+      </View>
+    </Modal>
   }
 
   return(
     <>
+      <TheModal/>
       <Text style={styles.header}>Users</Text>
-      {/* <FlatList
-        data={list}
+      <FlatList
+        data={userList}
         renderItem={renderUser}
-        keyExtractor={(item) => item.email}
+        keyExtractor={(item) => item?.email}
         style={styles.container}
         ItemSeparatorComponent={() => <View style={styles.seperator} />}
-      /> */}
-      <ScrollView style={styles.container}>
-        {list.map(renderUser)}
-      </ScrollView>
+        ListEmptyComponent={()=> {
+          return <>
+          {
+            error ? <Text>
+              Something went wrong
+            </Text> : <ActivityIndicator />
+          }
+          </>
+        }}
+      />
     </>
   );
 }
@@ -80,6 +143,36 @@ const styles = StyleSheet.create({
     width: 50,
     marginRight: 20,
     borderRadius: 25
+  },
+  modalConainer:{
+    paddingHorizontal:20,
+    paddingTop:40,
+    paddingBottom:20,
+    width: "100%",
+    marginRight:"20%",
+    marginLeft:"5%",
+    backgroundColor:"darkslategrey",
+    position:"absolute",
+    top: "30%",
+    borderRadius:10,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  modalName:{
+    textAlign:"center"
+  },
+  modalEmail:{
+    textAlign:"center"
+  },
+  modalAddress:{
+    textAlign:"center",
+    marginBottom:10
+  },
+  modalBtnStyle:{
+    backgroundColor: "transparent",
+    position: "absolute",
+    right: 10,
+    top: 5
   }
 });
 
