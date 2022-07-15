1a1160205d1702f27374de7a1503ccf0c1733da0
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-07-15 06:56:13 +0530

    test cases

--- App.tsx
+++ App.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, StatusBar, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, StatusBar, View, Button, TextInput, FlatList, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import ClsComponent from './src/screens/ClassComponentScreen';
 import FnComoponent from './src/screens/FunctionalComponentScreen';
 
 const App = () => {
+  const  [text, setText] = useState("")
+  const [theList, setList] = useState<string[]>([])
+
+  const navigation = useNavigation()
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setList(["a", "b", "c"])
+    }, 5000)
+  },[])
 
   return (
     <View style={styles.container}>
       <SafeAreaView />
       <StatusBar barStyle="default" />
       {/* <ClsComponent /> */}
-      <FnComoponent />
+      {/* <FnComoponent /> */}
+      <TextInput 
+        testID='a'
+        placeholder='Enter something'
+        onChangeText={(txt)=> {
+          setText(txt);
+        }}
+      />
+      <FlatList 
+        data={theList}
+        renderItem={({item, index})=> {
+          return <Button
+          testID={`txt-${index}`}
+          title='Press Me'
+          onPress={()=>{
+            console.log("pressed")
+            navigation.isFocused()
+          }}
+          />
+        }}
+      />
     </View>
   );
 };
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -7,8 +7,22 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
+import {fireEvent, render} from "@testing-library/react-native"
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+jest.mock("@react-navigation/native", ()=>{
+  return {
+    useNavigation: ()=> jest.fn()
+  }
+})
+
+it('renders correctly', async() => {
+  jest.setTimeout(7000);
+  const screen = render (<App />);
+  const input = screen.getByPlaceholderText("Enter something")
+  fireEvent.changeText(input, "asdf")
+  // const btn = screen.getByRole("button");
+  await new Promise((r)=> setTimeout(r,5000))
+  const btn = screen.getByTestId("txt-2")
+  fireEvent.press(btn);
 });
